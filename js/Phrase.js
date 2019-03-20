192beb4cee062962806be1976cd7bed8b32da7f7
Daniel Salas <danielsalas074@gmail.com>
2019-03-20 00:35:00 -0700

    added check letter method in the phrase class

--- js/Phrase.js
+++ js/Phrase.js
@@ -20,4 +20,16 @@ class Phrase {
     });
 }
 
+checkLetter(letter)
+{
+  return this.phrase.includes(letter);
+}
+showMatchedLetter()
+{
+  /*Reveals the letter(s) on the board that matches the
+player's selection. To reveal the matching letter(s), select all of the letter DOM
+elements that have a CSS class name that matches the selected letter and
+replace each selected element's `hide` CSS class with the `show` CSS class.*/
+}
+
 }
